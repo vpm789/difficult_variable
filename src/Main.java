e6a139d61c35163679dd3f1e68eafa32f790f15b
vpm789 <vpm789@gmail.com>
2023-02-09 23:40:59 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task6();
+        task7();
+        task8();
     }
+
+    public static void task6() {
+        System.out.println("");
+        System.out.println("Задача №6");
+        int a = 12;
+        int b = 27;
+        int c = 44;
+        int d = 15;
+        int e = 9;
+        int result = a * (b + (c - d * e));
+        System.out.println(-result);
+    }
+
+    public static void task7() {
+        System.out.println("");
+        System.out.println("Задача №7");
+        int a = 5;
+        int b = 7;
+        a = a * b;
+        b = a / b;
+        a = a / b;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+    public static void task8() {
+        System.out.println("");
+        System.out.println("Задача №8");
+        int a = 977;
+        int b = (a - a / 100 * 100) / 10;
+        System.out.println(b);
+    }
+
 }
